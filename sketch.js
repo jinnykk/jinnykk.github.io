878d421945f24a934b9aec34489dadb6aedceb27
jinnykk <92740486+jinnykk@users.noreply.github.com>
2021-10-19 00:10:46 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var imgs = [];
+var imageWidth;
+var imageHeight;
+var imageToUse = 0;
+
+function preload() {
+  imgs[0] = loadImage( "hand.jpg" );
+  imgs[1] = loadImage( "herhand.jpg" );
+  imgs[2] = loadImage( "type.jpg" );
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //   Adjust frame rate
+  frameRate(12);
+  //   Adjust Image Size
+  imageWidth = imgs[0].width / 5;
+  imageHeight = imgs[0].height / 3;
+  
+  imgs[0].filter(GRAY);
+  imgs[1].filter(GRAY);
+  imgs[2].filter(GRAY);
+  imageMode(CENTER);
+}
+
+function draw() {
+  //   Change Background Color
+  background(255,255,255,10);
+  tint(40, 80, 90)
+  
+  image(imgs[imageToUse], mouseX, mouseY,imageWidth, imageHeight);
+  imageToUse = (imageToUse+1) % imgs.length;
+}
